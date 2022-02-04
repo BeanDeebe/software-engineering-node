c5302d6af1152959396639a3400ff0b2bc86462e
Dean Beebe <beebe.d@northeastern.edu>
2022-02-04 17:45:50 -0500

    Working on A1 part III

--- server.js
+++ server.js
@@ -2,8 +2,7 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 mongoose.connect('mongodb+srv://beebed:<Patriots2020!>@cluster0.onmpj.mongodb.net/myFirstDatabase?retryWrites=true&w=majority')
-app.get('/hello', (req, res) =>
-  res.send('Hello World!'));
+
 
 const PORT = 4000;
 app.listen(PORT);

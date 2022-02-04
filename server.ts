d037c16f61e933f4dda6a4009bcca25c9c05786f
Dean Beebe <beebe.d@northeastern.edu>
2022-02-04 17:53:16 -0500

    Working on A1 part III

--- server.ts
+++ server.ts
@@ -1,8 +1,6 @@
 import express from 'express';
-const app = express();
 
-app.get('/users', (req, res) =>
-    res.send('Hello World!'));
+const app = express();
 
 const PORT = 4000;
 app.listen(process.env.PORT || PORT);

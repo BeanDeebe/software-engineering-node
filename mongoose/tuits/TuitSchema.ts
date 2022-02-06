19fbdfa72576e2ae17b38a55f3727198fa41b28c
Dean Beebe <beebe.d@northeastern.edu>
2022-02-06 11:36:09 -0500

    Part III A1

--- mongoose/tuits/TuitSchema.ts
+++ mongoose/tuits/TuitSchema.ts
@@ -5,7 +5,7 @@ import User from "../../models/users/User";
 const TuitSchema = new mongoose.Schema ({
     tuit: {type: String, required: true},
     postedOn: {type: Date, default: Date.now()},
-    postedBy: {type: String, ref:"UserModel.username"}
+    postedBy: {type: User, ref:"UserModel"}
 }, {collection: 'tuits'});
 
 export default TuitSchema;
